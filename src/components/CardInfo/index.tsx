807253e8788bae28925ca40bcf3de843466a9ff3
Ianalas <alasian510@gmail.com>
2025-04-07 09:49:08 -0300

    Fix: Name and Login apparecen and Keys Auth API hidden

--- src/components/CardInfo/index.tsx
+++ src/components/CardInfo/index.tsx
@@ -4,17 +4,19 @@ import { UserQueryContext } from "../../context/user-query-context";
 
 export function CardInfo() {
   const { profile, isFounded, hasSearched } = useContext(UserQueryContext);
-  const { name, bio, avatar_url } = profile;
+  const { name, bio, avatar_url, login } = profile;
 
   if (!hasSearched) return null; 
+
+  console.log(name);
 
   return (
     <ContainerCardInfo>
       {isFounded ? (
         <div>
-          <img src={avatar_url} alt={`Avatar de ${name}`} />
+          <img src={avatar_url} alt={`Avatar de ${name ? name : login}`} />
           <div>
-            <h2>{name}</h2>
+            <h2>{name ? name : login}</h2>
             <p>{!bio ? "Este usuário não tem uma bio! =(" : bio}</p>
           </div>
         </div>
